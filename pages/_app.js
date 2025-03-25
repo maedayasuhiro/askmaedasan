fe0ac76083a1f86dd61f6ea13d86a588e185f933
maedayasuhiro <m.yattukun@icloud.com>
2025-03-25 19:15:23 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* 和風フォント Noto Serif JP 読み込み */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;

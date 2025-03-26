c97c78da4122fd7805128988f7f2f782287406a4
maedayasuhiro <m.yattukun@icloud.com>
2025-03-26 15:22:42 +0000

    Update _documents.js

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,10 +1,11 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        {/* Instagram Embed Script */}
+        {/* Instagram 埋め込み用 SDK */}
         <script async defer src="https://www.instagram.com/embed.js"></script>
       </Head>
       <body>

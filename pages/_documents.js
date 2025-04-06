e7bd5ecc6192525769c37ad2adb46653e085f1fa
maedayasuhiro <m.yattukun@icloud.com>
2025-04-06 13:38:08 +0100

    Update _documents.js

--- pages/_documents.js
+++ pages/_documents.js
@@ -3,8 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
+        {/* ファビコン */}
+        <link rel="icon" href="/logo.png" type="image/png" />
+
+        {/* ブラウザのテーマカラー（オプション） */}
+        <meta name="theme-color" content="#E34234" />
+        <meta name="msapplication-TileColor" content="#E34234" />
+
         {/* Instagram 埋め込み用 SDK */}
         <script async defer src="https://www.instagram.com/embed.js"></script>
       </Head>
